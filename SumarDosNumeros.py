b7f1e7f447d99f34502ff9a4c39b2a3a46a64848
Guillermo Arguello Cordobe <74156373+Elmemoing@users.noreply.github.com>
2021-08-25 19:41:19 -0400

    Sumar dos números

--- SumarDosNumeros.py
+++ SumarDosNumeros.py
@@ -1,11 +1,10 @@
 #Hacer un programa para sumar dos números leídos por teclado y escribir el resultado
 
-a = int(input("Ingrese un numero"))
-b = int(input("Ingrese otro numero"))
+numero_uno = int(input("Ingrese un numero"))
+numero_dos = int(input("Ingrese otro numero"))
 
 def suma(a,b):
   suma = (a+b)
   return suma
 
-print(suma(a,b))
-
+print(suma(numero_uno,numero_dos))
