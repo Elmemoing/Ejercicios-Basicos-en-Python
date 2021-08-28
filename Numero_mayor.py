cc3e948b89514beee7279365f83e4fe342b2b296
Guillermo Arguello Cordobe <74156373+Elmemoing@users.noreply.github.com>
2021-08-28 16:16:24 -0400

    Cual numero es mayor

---
+++ Numero_mayor.py
@@ -0,0 +1,13 @@
+"""Hacer un programa en Python que permita leer 2 números diferentes
+y nos diga cual es el mayor de los 2 números"""
+
+num1 = int(input("ingrese un numero "))
+num2 = int(input("Ingrese otro numero "))
+
+def mayor(a,b):
+  if a>b
+    return(a,"es mayor")
+  else:
+    return(b,"es mayor")
+  
+print(mayor(num1,num2))
