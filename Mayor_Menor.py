685585099a83c1110109843588b565fdd333344c
Guillermo Arguello Cordobe <74156373+Elmemoing@users.noreply.github.com>
2021-08-29 23:48:10 -0400

    Mayor y Menor entre 3 números

--- Mayor_Menor.py
+++ Mayor_Menor.py
@@ -2,31 +2,30 @@
 Crear un programa en Python en el que se 
 almacenen 3 números en 3 variables A, B y C. El diagrama debe decidir cuál es el mayor y cual es el menor
 """
-
-A = float(input("Ingrese un numero"))
-B = float(input("Ingrese otro numero")
-C = float(input("Ingrese un tercer numero"))
-
-  def mayor(N1,N2,N3):
+def mayor(N1,N2,N3):
           if N1>N2 and N1>N3:
             return N1
           elif N2>N1 and N2>N3:
             return N2
-          else
+          else:     
             return N3
           
-  def menor(N1,N2,N3):
+def menor(N1,N2,N3):
           if N1<N2 and N1<N3:
             return N1
           elif N2<N1 and N2<N3:
             return N2
           else:
             return N3
-          
- Diferentes = A != B and B != C and A != C
-          
-          if diferentes:
-              print("mayor es: ", mayor(A,B,C))
-              print("menor es: ", mayor(A,B,C))
-          else:
-              print("Los numeros deben ser diferentes")
+
+A = float(input("Ingrese un numero: "))
+B = float(input("ingrese un numero: "))
+C = float(input("Ingrese un tercer numero: "))
+
+Diferentes = A != B and B != C and A != C
+
+if Diferentes:
+    print("mayor es: ", mayor(A,B,C))
+    print("menor es: ", menor(A,B,C))
+else:
+    print("Los numeros deben ser diferentes")
